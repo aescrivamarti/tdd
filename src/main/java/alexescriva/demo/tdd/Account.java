4db5aec74c5b53352474bcaaec3a0b3ef5c149d3
aescrivamarti <alesma05@floridauniversitaria.es>
2025-04-09 10:56:27 +0200

    Third commit - Use cases (transfers)

--- src/main/java/alexescriva/demo/tdd/Account.java
+++ src/main/java/alexescriva/demo/tdd/Account.java
@@ -16,11 +16,17 @@ public class Account {
         }
     }
 
-    public void withrawal(int amount) {
-        if (amount < balance && amount > 0 && amount < 3000) {
+    public void withdrawal(int amount) {
+        if (amount <= balance && amount > 0 && amount <= 3000) {
             balance -= amount;
         }
     }
 
+    public void transfer(int amount, Account destinationaccount) {
+        if (amount < balance && amount > 0 && amount <= 1500) {
+            balance -= amount;
+            destinationaccount.deposit(amount);
+        }
+    }
 }
 
--- src/test/java/alexescriva/demo/tdd/TestAccount.java
+++ src/test/java/alexescriva/demo/tdd/TestAccount.java
@@ -63,51 +63,138 @@ public class TestAccount {
     }
 
     @Test
-    void withrawalSetsBalanceTo490WhenWithrawing10InAccountWith500(){
+    void withdrawalSetsBalanceTo490WhenWithdrawing10InAccountWith500(){
         Account account = new Account();
         account.deposit(500);
-        account.withrawal(10);
+        account.withdrawal(10);
 
         assertEquals(490, account.getBalance());
     }
 
     @Test
-    void withrawalSetsBalanceTo100WhenWithrawing300InAccountWith100(){
+    void withdrawalSetsBalanceTo100WhenWithdrawing300InAccountWith100(){
         Account account = new Account();
         account.deposit(100);
-        account.withrawal(300);
+        account.withdrawal(300);
 
         assertEquals(100, account.getBalance());
     }
 
     @Test
-    void withrawalSetsBalanceTo300WhenWithrawingNegative10InAccountWith300(){
+    void withdrawalSetsBalanceTo300WhenWithdrawingNegative10InAccountWith300(){
         Account account = new Account();
         account.deposit(300);
-        account.withrawal(-10);
+        account.withdrawal(-10);
 
         assertEquals(300, account.getBalance());
     }
 
     @Test
-    void withrawalSetsBalanceTo1000WhenWithrawing3000InAccountWith4000(){
+    void withdrawalSetsBalanceTo1000WhenWithdrawing3000InAccountWith4000(){
         Account account = new Account();
         account.deposit(2000);
         account.deposit(2000);
-        account.withrawal(3000);
+        account.withdrawal(3000);
 
         assertEquals(1000, account.getBalance());
     }
 
     @Test
-    void withrawalSetsBalanceTo4000WhenWithrawing3001InAccountWith4000(){
+    void withdrawalSetsBalanceTo4000WhenWithdrawing3001InAccountWith4000(){
         Account account = new Account();
         account.deposit(2000);
         account.deposit(2000);
-        account.withrawal(3001);
+        account.withdrawal(3001);
 
         assertEquals(4000, account.getBalance());
     }
 
+    @Test
+    void transferSetsSourceAccountBalanceTo290AndDestinationAccount410() {
+        Account sourceaccount = new Account();
+        sourceaccount.deposit(300);
+        Account destinationaccount = new Account();
+        destinationaccount.deposit(400);
+
+        sourceaccount.transfer(10, destinationaccount);
+
+        assertEquals(290, sourceaccount.getBalance());
+        assertEquals(410, destinationaccount.getBalance());
+    }
+
+    @Test
+    void transferSetsSourceAccountBalanceTo200AndDestinationAccount400() {
+        Account sourceaccount = new Account();
+        sourceaccount.deposit(300);
+        Account destinationaccount = new Account();
+        destinationaccount.deposit(400);
+
+        sourceaccount.transfer(100, destinationaccount);
+
+        assertEquals(200, sourceaccount.getBalance());
+        assertEquals(500, destinationaccount.getBalance());
+    }
+
+    @Test
+    void transferSetsSourceAccountBalanceTo300AndDestinationAccount40() {
+        Account sourceaccount = new Account();
+        sourceaccount.deposit(300);
+        Account destinationaccount = new Account();
+        destinationaccount.deposit(40);
+
+        sourceaccount.transfer(-10, destinationaccount);
+
+        assertEquals(300, sourceaccount.getBalance());
+        assertEquals(40, destinationaccount.getBalance());
+    }
+
+    @Test
+    void transferSetsSourceAccountBalanceTo1000AndDestinationAccount2000() {
+        Account sourceaccount = new Account();
+        sourceaccount.deposit(2500);
+        Account destinationaccount = new Account();
+        destinationaccount.deposit(500);
+
+        sourceaccount.transfer(1500, destinationaccount);
+
+        assertEquals(1000, sourceaccount.getBalance());
+        assertEquals(2000, destinationaccount.getBalance());
+    }
+
+    @Test
+    void transferSetsSourceAccountBalanceTo2500AndDestinationAccount500() {
+        Account sourceaccount = new Account();
+        sourceaccount.deposit(2500);
+        Account destinationaccount = new Account();
+        destinationaccount.deposit(500);
+
+        sourceaccount.transfer(1501, destinationaccount);
+
+        assertEquals(2500, sourceaccount.getBalance());
+        assertEquals(500, destinationaccount.getBalance());
+    }
+
+    @Test
+    void transferSetsSourceAccountBalanceTo300AndDestinationAccount2700() {
+        Account sourceaccount = new Account();
+        sourceaccount.deposit(2500);
+        Account destinationaccount = new Account();
+        destinationaccount.deposit(500);
+
+        sourceaccount.transfer(1000, destinationaccount);
+        sourceaccount.transfer(1200, destinationaccount);
+
+        assertEquals(300, sourceaccount.getBalance());
+        assertEquals(2700, destinationaccount.getBalance());
+    }
+
+    @Test
+    void withdrawalSetsBalanceTo0WhenWithdrawing1000InAccountWith1000(){
+        Account account = new Account();
+        account.deposit(1000);
+        account.withdrawal(1000);
+
+        assertEquals(0, account.getBalance());
+    }
 }
 
